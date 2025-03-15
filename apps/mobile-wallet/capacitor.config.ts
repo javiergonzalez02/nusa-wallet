f13c5a5c4671c173122c510171827cad28fbd337
javiergonzalez02 <javi.galeon2012@gmail.com>
2025-03-15 20:05:21 +0100

    feat: change wallet name and edit .gitignore

--- apps/mobile-wallet/capacitor.config.ts
+++ apps/mobile-wallet/capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'mobile-wallet',
+  appId: 'nusa.wallet.mobile',
+  appName: 'Nusa',
   webDir: 'dist'
 };
 

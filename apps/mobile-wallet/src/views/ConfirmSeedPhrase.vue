8c26278d6a38acec7ca0c74380172080979b5da8
javiergonzalez02 <javi.galeon2012@gmail.com>
2025-03-24 22:40:28 +0100

    feat: redirect to MainView when Mnemonic phrase is confirmed

--- apps/mobile-wallet/src/views/ConfirmSeedPhrase.vue
+++ apps/mobile-wallet/src/views/ConfirmSeedPhrase.vue
@@ -6,8 +6,8 @@
         <ion-item v-for="i in 6" :key="i">
           <ion-label position="stacked">Word {{ i }}</ion-label>
           <ion-input
-            v-model="seedWords[i - 1]"
-            :placeholder="`Enter word ${i}`"
+              v-model="seedWords[i - 1]"
+              :placeholder="`Enter word ${i}`"
           ></ion-input>
         </ion-item>
       </div>
@@ -15,8 +15,8 @@
         <ion-item v-for="i in 6" :key="i + 6">
           <ion-label position="stacked">Word {{ i + 6 }}</ion-label>
           <ion-input
-            v-model="seedWords[i + 5]"
-            :placeholder="`Enter word ${i + 6}`"
+              v-model="seedWords[i + 5]"
+              :placeholder="`Enter word ${i + 6}`"
           ></ion-input>
         </ion-item>
       </div>
@@ -34,7 +34,9 @@ import { IonButton, IonInput, IonItem, IonLabel, IonList } from '@ionic/vue';
 import BaseLayout from '../layouts/BaseLayout.vue';
 import { useSeedStore } from '@/stores/seedStore';
 import { setSeedPhrase } from '@/utils/secureStorage/seed';
+import { useRouter } from "vue-router";
 
+const router = useRouter();
 const seedStore = useSeedStore();
 const originalSeed = computed(() => seedStore.mnemonic);
 
@@ -42,7 +44,7 @@ const originalSeed = computed(() => seedStore.mnemonic);
 const seedWords = ref<string[]>(new Array(12).fill(''));
 const errorMessage = ref('');
 
-const confirmSeed = async () => {
+const confirmSeed = async() => {
   // Join the input words into a single string.
   const inputSeed = seedWords.value.join(' ').trim();
   const expectedSeed = originalSeed.value.trim();
@@ -51,6 +53,7 @@ const confirmSeed = async () => {
     try {
       await setSeedPhrase(expectedSeed);
       alert('Seed phrase saved successfully!');
+      await router.push({ name: 'mainview' });
     } catch (error) {
       errorMessage.value = 'Error saving seed phrase to secure storage.';
       console.error('Error:', error);

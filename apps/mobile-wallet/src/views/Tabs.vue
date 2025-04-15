199f33842c8976f4507ff6434b750d1c01f1fe2a
javiergonzalez02 <javi.galeon2012@gmail.com>
2025-04-15 13:22:31 +0200

    fix: wrap Tabs content in IonPage to fix routing

--- apps/mobile-wallet/src/views/Tabs.vue
+++ apps/mobile-wallet/src/views/Tabs.vue
@@ -1,22 +1,24 @@
 <template>
-  <ion-tabs>
-    <ion-router-outlet></ion-router-outlet>
-    <ion-tab-bar slot="bottom">
-      <ion-tab-button tab="dashboard" href="/tabs/dashboard">
-        <ion-icon :icon="speedometerOutline"/>
-        <ion-label>Dashboard</ion-label>
-      </ion-tab-button>
-      <ion-tab-button tab="settings" href="/tabs/settings">
-        <ion-icon :icon="settingsOutline"/>
-        <ion-label>Settings</ion-label>
-      </ion-tab-button>
-    </ion-tab-bar>
-  </ion-tabs>
+  <ion-page>
+    <ion-tabs>
+      <ion-router-outlet></ion-router-outlet>
+      <ion-tab-bar slot="bottom">
+        <ion-tab-button tab="dashboard" href="/tabs/dashboard">
+          <ion-icon :icon="speedometerOutline"/>
+          <ion-label>Dashboard</ion-label>
+        </ion-tab-button>
+        <ion-tab-button tab="settings" href="/tabs/settings">
+          <ion-icon :icon="settingsOutline"/>
+          <ion-label>Settings</ion-label>
+        </ion-tab-button>
+      </ion-tab-bar>
+    </ion-tabs>
+  </ion-page>
 </template>
-
 
 <script setup lang="ts">
 import {
+  IonPage,
   IonTabs,
   IonTabBar,
   IonTabButton,

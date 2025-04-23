8c0b627da700587614d764edf06768ee1fc3a012
javiergonzalez02 <javi.galeon2012@gmail.com>
2025-04-23 16:06:21 +0200

    feat: add ImportSeed in routing and add 'next' prop in create pass so it knows where to go next

--- apps/mobile-wallet/src/router/index.ts
+++ apps/mobile-wallet/src/router/index.ts
@@ -7,57 +7,66 @@ import ConfirmSeedPhrase from "@/views/ConfirmSeedPhrase.vue";
 import Settings from "@/views/Settings.vue";
 import Tabs from "@/views/Tabs.vue";
 import Dashboard from "@/views/Dashboard.vue";
+import ImportSeed from "@/views/ImportSeed.vue";
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    redirect: '/welcome'
-  },
-  {
-    path: '/welcome',
-    name: 'welcome',
-    component: WelcomePage
-  },
-  {
-    path: '/createpass',
-    name: 'createpass',
-    component: CreatePassword
-  },
-  {
-    path: '/createseed',
-    name: 'createseed',
-    component: CreateSeedPhrase
-  },
-  {
-    path: '/confirmseed',
-    name: 'confirmseed',
-    component: ConfirmSeedPhrase
-  },
-  {
-    path: '/tabs/',
-    component: Tabs,
-    children: [
-      {
-        path: '',
-        redirect: '/tabs/dashboard'
-      },
-      {
-        path: 'dashboard',
-        name: 'dashboard',
-        component: Dashboard
-      },
-      {
-        path: 'settings',
-        name: 'settings',
-        component: Settings
-      }
-    ]
-  }
+	{
+		path: '/',
+		redirect: '/welcome'
+	},
+	{
+		path: '/welcome',
+		name: 'welcome',
+		component: WelcomePage
+	},
+	{
+		path: '/createpass',
+		name: 'createpass',
+		component: CreatePassword,
+		props: route => ({
+			next: (route.query.next as string) || 'createseed'
+		})
+	},
+	{
+		path: '/createseed',
+		name: 'createseed',
+		component: CreateSeedPhrase
+	},
+	{
+		path: '/confirmseed',
+		name: 'confirmseed',
+		component: ConfirmSeedPhrase
+	},
+	{
+		path: '/importseed',
+		name: 'importseed',
+		component: ImportSeed
+	},
+	{
+		path: '/tabs/',
+		component: Tabs,
+		children: [
+			{
+				path: '',
+				redirect: '/tabs/dashboard'
+			},
+			{
+				path: 'dashboard',
+				name: 'dashboard',
+				component: Dashboard
+			},
+			{
+				path: 'settings',
+				name: 'settings',
+				component: Settings
+			}
+		]
+	}
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes
 })
 
 export default router
